a15d97d77f6066dea6a6bf42d469d218aee3c3cf
Scott Standaert <scott.standaert@gmail.com>
2022-07-29 11:44:35 -0700

    finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR';
+
+    for (let i = num; i <= num; i++) {
+        return string.repeat(num);
+    }
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
