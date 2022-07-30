a5020340dbba298f0a6e397bd24d082fc7aa56cc
Scott Standaert <scott.standaert@gmail.com>
2022-07-29 18:35:03 -0700

    complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split('');
+    stringArray.reverse();
+    return stringArray.join('');
 };
 
 // Do not edit below this line
