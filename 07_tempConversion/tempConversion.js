c13c0b8ec4c27d4dc8a9284a7a1534dafa8cd1c6
Scott Standaert <scott.standaert@gmail.com>
2022-07-30 22:55:25 -0700

    finsh exercise 6 + 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  const ftemp = (temp - 32) * 5/9;
+  return Math.round(ftemp * 10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  const ctemp = (temp * 9/5) + 32;
+  return Math.round(ctemp * 10)/10;
 };
 
 // Do not edit below this line
