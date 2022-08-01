c317c1bdb4c35be2f741abde63394ee8a25590f6
Scott Standaert <scott.standaert@gmail.com>
2022-08-01 00:35:56 -0700

    finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    //checks if min or max are integers, returns error if they are not a integer
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR';
+    // returns error if min or max are less then 0(negative numbers)
+    if (min < 0 || max <0) return 'ERROR';
+    //if min, the starting value is greater then max the end value of the range then turn min into max and max into temp which is same as min
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+    }
+    //loops through the range and sums each number
+    let sum = 0;
+    for (let i = min; i < max + 1; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
